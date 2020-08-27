aab8a862d991fa00d046e48c8a94c87f128d1c46
sj0rs1 <sjburhennemail@gmail.com>
2020-08-28 00:44:42 +0200

    Create Decoder.lua

---
+++ Decoder.lua
@@ -0,0 +1,119 @@
+local alphabet = {
+    a = "😳🔥🔥😳😳😳😳🔥",
+    b = "😳🔥🔥😳😳😳🔥😳",
+    c = "😳🔥🔥😳😳😳🔥🔥",
+    d = "😳🔥🔥😳😳🔥😳😳",
+    e = "😳🔥🔥😳😳🔥😳🔥",
+    f = "😳🔥🔥😳😳🔥🔥😳",
+    g = "😳🔥🔥😳😳🔥🔥🔥",
+    h = "😳🔥🔥😳🔥😳😳😳",
+    i = "😳🔥🔥😳🔥😳😳🔥",
+    j = "😳🔥🔥😳🔥😳🔥😳",
+    k = "😳🔥🔥😳🔥😳🔥🔥",
+    l = "😳🔥🔥😳🔥🔥😳😳",
+    m = "😳🔥🔥😳🔥🔥😳🔥",
+    n = "😳🔥🔥😳🔥🔥🔥😳",
+    o = "😳🔥🔥😳🔥🔥🔥🔥",
+    p = "😳🔥🔥🔥😳😳😳😳",
+    q = "😳🔥🔥🔥😳😳😳🔥",
+    r = "😳🔥🔥🔥😳😳🔥😳",
+    s = "😳🔥🔥🔥😳😳🔥🔥",
+    t = "😳🔥🔥🔥😳🔥😳😳",
+    u = "😳🔥🔥🔥😳🔥😳🔥",
+    v = "😳🔥🔥🔥😳🔥🔥😳",
+    w = "😳🔥🔥🔥😳🔥🔥🔥",
+    x = "😳🔥🔥🔥🔥😳😳😳",
+    y = "😳🔥🔥🔥🔥😳😳🔥",
+    z = "😳🔥🔥🔥🔥😳🔥😳",
+    one = "😳😳🔥🔥😳😳😳🔥",
+    two = "😳😳🔥🔥😳😳🔥😳",
+    three = "😳😳🔥🔥😳😳🔥🔥",
+    four = "😳😳🔥🔥😳🔥😳😳",
+    five = "😳😳🔥🔥😳🔥😳🔥",
+    six = "😳😳🔥🔥😳🔥🔥😳",
+    seven = "😳😳🔥🔥😳🔥🔥🔥",
+    eight = "😳😳🔥🔥🔥😳😳😳",
+    nine = "😳😳🔥🔥🔥😳😳🔥",
+    zero = "😳😳🔥🔥😳😳😳😳",
+    plus = "😳😳🔥😳🔥😳🔥🔥",
+    minus = "😳😳🔥😳🔥🔥😳🔥",
+    forwardslash = "😳😳🔥😳🔥🔥🔥🔥", -- /
+    asterisk = "😳😳🔥😳🔥😳🔥😳", -- *
+    percentage = "😳😳🔥😳😳🔥😳🔥",
+    andsign = "😳😳🔥😳😳🔥🔥😳",
+    openbracket = "😳😳🔥😳🔥😳😳😳",
+    closebracket = "😳😳🔥😳🔥😳😳🔥",
+    dot = "😳😳🔥😳🔥🔥🔥😳",
+    string1 = "😳😳🔥😳😳😳🔥😳",
+    string2 = "😳😳🔥😳😳🔥🔥🔥",
+    squareopenbracket = "😳🔥😳🔥🔥😳🔥🔥",
+    weirdopenbracket = "😳🔥🔥🔥🔥😳🔥🔥",
+    squareclosebracket = "😳🔥😳🔥🔥🔥😳🔥",
+    weirdclosebracket = "😳🔥🔥🔥🔥🔥😳🔥",
+}
+
+local decode = _G.decode
+local loadedString = ""
+for i=1,string.len(decode)/32 do
+    local letter = string.sub(decode,32*i-32,32*i)
+    for i,v in pairs(alphabet) do
+        if string.len(v) ~= string.len(letter) then letter = string.sub(letter,2,33) end
+        if v == letter then
+            local letter = i
+            if letter == "one" then
+                letter = "1"
+            elseif letter == "two" then
+                letter = "2"
+            elseif letter == "three" then
+                letter = "3"
+            elseif letter == "four" then
+                letter = "4"
+            elseif letter == "five" then
+                letter = "5"
+            elseif letter == "six" then
+                letter = "6"
+            elseif letter == "seven" then
+                letter = "7"
+            elseif letter == "eight" then
+                letter = "8"
+            elseif letter == "nine" then
+                letter = "9"
+            elseif letter == "zero" then
+                letter = "0"
+            elseif letter == "plus" then
+                letter = "+"
+            elseif letter == "minus" then
+                letter = "-"
+            elseif letter == "forwardslash" then
+                letter = "/"
+            elseif letter == "asterisk" then
+                letter = "*"
+            elseif letter == "percentage" then
+                letter = "%"
+            elseif letter == "andsign" then
+                letter = "&"
+            elseif letter == "openbracket" then
+                letter = "("
+            elseif letter == "closebracket" then
+                letter = ")"
+            elseif letter == "dot" then
+                letter = "."
+            elseif letter == "string1" then
+                letter = '"'
+            elseif letter == "string2" then
+                letter = "'"
+            elseif letter == "squareopenbracket" then
+                letter = "["
+            elseif letter == "weirdopenbracket" then
+                letter = "{" 
+            elseif letter == "squareclosebracket" then
+                letter = "]" 
+            elseif letter == "weirdclosebracket" then
+                letter = "}" 
+            end
+            loadedString = loadedString..letter
+        end
+    end
+end
+
+loadstring(loadedString)()
